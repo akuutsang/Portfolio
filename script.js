cc45d2129b840b7f12e0d4f6101e69a59b7b696f
Akutsang <mymac@MYs-MacBook-Pro.local>
2023-04-22 19:39:08 +0100

    set live clock, hour, minute and seecond

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 const hour = document.querySelector(".hour");
 const minute = document.querySelector(".minute");
+const second = document.querySelector(".second")
+const updateTime = () => {
+    const currentTime = new Date();
+
+    let currentHour = currentTime.getHours();
+    let currentMinute = currentTime.getMinutes();
+    let currentSecond = currentTime.getSeconds()
+if (currentHour > 12) {
+currentHour -= 12;
+}
+    hour.textContent = currentHour.toString();
+    minute.textContent = currentMinute.toString().padStart(2,"0");
+    second.textContent = currentSecond.toString()
+}
+setInterval(updateTime, 1000);
+updateTime();
