5e28c1029ef7ac488c9c963caf793e58eacda8fb
Akutsang <mymac@MYs-MacBook-Pro.local>
2023-05-04 11:49:42 +0100

    add about section

--- script.js
+++ script.js
@@ -1,19 +1,4 @@
-const hour = document.querySelector(".hour");
-const minute = document.querySelector(".minute");
-const second = document.querySelector(".second")
+import { updateTime, card } from './utils.js';
 
-const updateTime = () => {
-    const currentTime = new Date();
-
-    let currentHour = currentTime.getHours();
-    let currentMinute = currentTime.getMinutes();
-    let currentSecond = currentTime.getSeconds()
-if (currentHour > 12) {
-currentHour -= 12;
-}
-    hour.textContent = currentHour.toString();
-    minute.textContent = currentMinute.toString().padStart(2,"0");
-    second.textContent = currentSecond.toString()
-}
-setInterval(updateTime, 1000);
 updateTime();
+card();
